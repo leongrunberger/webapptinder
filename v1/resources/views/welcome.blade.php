6f1eae31d9da4a38149fc27102ba80c0884735fa
roflcopter600000 <koch.pascal_95@web.de>
2020-02-11 18:18:45 +0100

    Title geändert

--- v1/resources/views/welcome.blade.php
+++ v1/resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Roflcopter60000
                 </div>
 
                 <div class="links">
